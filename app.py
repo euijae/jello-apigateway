fe22a0daac71be62f7990f40f52bf4bd71238c28
euijae <acreclan49@gmail.com>
2020-10-03 23:24:29 -0700

    add new api

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
-from flask import jsonify
+from flask import request, jsonify
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
@@ -25,12 +26,29 @@ books = [
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction " \
+           "novels.</p> "
 
 
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
     return jsonify(books)
+
+
+@app.route('/api/v1/resources/book', methods=['GET'])
+def api_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    results = []
+
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    return jsonify(results)
 
 
 if __name__ == '__main__':
